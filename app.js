9576fbd257a3ec3fcc8a38b3993dfdbc6b4d7f22
tizenkethur <you@example.com>
2023-01-14 15:34:33 +0100

    sidenav can be opened and closed

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-console.log('hali');
 const links = document.querySelectorAll('a');
 links.forEach((link) => {
   if (link.href === window.location.href) {
     link.classList.add('active');
   }
 });
+
+function openNav() {
+  document.getElementById('mySidenav').style.width = '250px';
+}
+
+function closeNav() {
+  document.getElementById('mySidenav').style.width = '0';
+}
