972e5af8b854562fc35009a31326b187f7047fee
tizenkethur <you@example.com>
2023-01-17 22:10:27 +0100

    side nav added to other pages too

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ links.forEach((link) => {
 });
 
 function openNav() {
-  document.getElementById('mySidenav').style.width = '250px';
+  document.getElementById('mySidenav').style.width = '300px';
 }
 
 function closeNav() {
